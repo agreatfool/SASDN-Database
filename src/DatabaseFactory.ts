aa7ade50ec10686b39652aae2f043728188ad6db
金佳瑛 <jinjiaying@jinjiayingdeMacBook-Air.local>
2017-12-03 16:24:20 +0800

    add ShardTable

--- src/DatabaseFactory.ts
+++ src/DatabaseFactory.ts
@@ -1,6 +1,9 @@
-import { Connection, createConnections, DatabaseType } from 'typeorm';
+import { Connection, createConnections, DatabaseType, ConnectionOptions } from 'typeorm';
 import { ClusterOptions } from './typeorm/ClusterOptions';
 import { crc32 } from 'crc';
+import * as LibFs from 'mz/fs';
+import * as LibPath from 'path';
+const glob = require('glob');
 
 export class DatabaseFactory {
 
@@ -16,6 +19,66 @@ export class DatabaseFactory {
   }
 
   /**
+   * Read given path to find ShardTable then copy & rewrite shardTableEntity
+   * @param entityPath 
+   */
+  async checkShardTable(entityPath: string): Promise<string[]> {
+    let entitiesPath: Set<string> = new Set();
+    try {
+      let files = glob.sync(entityPath);
+      for (const file of files) {
+        const fileNameExp = /[a-zA-Z0-9]+.js/;
+        const copyExp = /[a-zA-Z0-9]+_[0-9]+.js/;
+        const copyMatch = copyExp.exec(file);
+        if(copyMatch) {
+          continue;
+        }
+        const fileNameMatch = fileNameExp.exec(file)[0];
+        const fileName = fileNameMatch.substring(0, fileNameMatch.length - 3);
+        const givenPath = entityPath.split('*')[0];
+        const context = await LibFs.readFileSync(file, 'utf-8');
+        const regExp: RegExp = new RegExp(/.shardTable\(__filename\,\s[0-9]+\)/);
+        const matchResult: string[] = regExp.exec(context);
+        entitiesPath.add(`${givenPath}*.js`);
+        if (!matchResult)
+          continue;
+        const tablePath = LibPath.join(givenPath, `/shardTables/`);
+        entitiesPath.add(`${givenPath}**/*.js`);
+        try {
+          await LibFs.stat(tablePath);
+        } catch (error) {
+          await LibFs.mkdir(tablePath);
+        }
+        const matchText = matchResult[0];
+        const numExp: RegExp = new RegExp(/[0-9]+/);
+        let numberMatch: number = parseInt(numExp.exec(matchText)[0]);
+        const classExp: RegExp = new RegExp(/class\s\b[A-Za-z0-9]+\b/);
+        const className = classExp.exec(context)[0].replace('class ', '');
+        for (let i = 0; i < numberMatch; i++) {
+          try {
+            const newFilePath = `${givenPath}/shardTables/${fileName}_${i}.js`;
+            try {
+              await LibFs.stat(newFilePath);
+            } catch (error) {
+              await LibFs.copyFileSync(file, newFilePath, LibFs.constants.COPYFILE_EXCL);
+            }
+            const readFile = await LibFs.readFileSync(file, 'utf-8');
+            const newClassName = `${className}_${i}`;
+            await LibFs.writeFile(newFilePath, readFile.replace(new RegExp(className, 'gm'), newClassName));
+          } catch (error) {
+            console.log('caught error = ', error);
+            return [entityPath];
+          }
+        }
+      }
+    } catch (error) {
+      console.log('caught error = ', error);
+      return [entityPath];
+    }
+    return [...entitiesPath];
+  }
+
+  /**
    * Create Database cluster by options
    * @param options array of ClusterOptions
    */
@@ -23,6 +86,10 @@ export class DatabaseFactory {
     const clusters = this.clusters;
     try {
       for (let cluster of options) {
+        for(let conn of cluster.cluster) {
+          let finalPath:Set<string> = new Set();
+          let entities:string[] = conn.entities as string[];
+        }
         clusters.set(cluster.name, await createConnections(cluster.cluster));
       }
     } catch (error) {
